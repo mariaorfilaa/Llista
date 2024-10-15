02ed62fe28a8e4391b0514be62d3c4dfe81c81a5
mariaorfilaa <morfila19549@iesjoanramis.org>
2024-10-15 12:45:19 +0200

    Add files via upload

---
+++ Addtocart - Maria Orfila/js/script.js
@@ -0,0 +1,23 @@
+const input = document.getElementbyId("inputFiled")
+const boto =  document.getElementbyId("afegir")
+
+boto.addEventListener("click", function (){
+    console.log(input.value)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
