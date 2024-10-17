f447f220d37367e3b93bb730d107d362ba54303d
mariaorfilaa <morfila19549@iesjoanramis.org>
2024-10-17 08:53:25 +0200

    Add files via upload

--- Addtocart - Maria Orfila/js/script.js
+++ Addtocart - Maria Orfila/js/script.js
@@ -1,8 +1,20 @@
-const input = document.getElementbyId("inputFiled")
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const input = document.getElementbyId("inputField")
 const boto =  document.getElementbyId("afegir")
 
+const appConfiguracio = {
+    databaseURL: "https://llista-428ab-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp (appConfiguracio);
+const baseDades = getDatabase (app);
+const task = ref(baseDades, "tareas");
+
 boto.addEventListener("click", function (){
-    console.log(input.value)
+    push (task, input.value)
+    alert ("Afegir a la BD")
 
 })
 
